dd59a41ceb9e65496fc1c6af4d5a2c2a403ee14d
Madhu Murthy <msmurthy@msn.com>
2019-07-29 22:21:24 -0700

    Added Header and Footer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+  get_header();
   if( have_posts() ):
     while ( have_posts() ) : the_post(); ?>
       <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
@@ -8,5 +8,5 @@
   else:
     echo '<p>There are no posts!</p>';
   endif;
-
+  get_footer();
   ?>
