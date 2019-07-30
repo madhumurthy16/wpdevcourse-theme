a251aa265107735d0a430388cd55ef3985856929
Madhu Murthy <msmurthy@msn.com>
2019-07-30 14:11:35 -0700

    Added link to site title.

--- header.php
+++ header.php
@@ -8,6 +8,6 @@
 
 <body <?php body_class(); ?>>
   <header class="site-header">
-    <h1><?php bloginfo('name'); ?></h1>
+    <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
     <h4><?php bloginfo('description'); ?></h4>
   </header>
