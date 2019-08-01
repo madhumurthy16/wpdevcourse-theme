e4ac15eaf37fea7e96deb09f95a3281fb1d65940
Madhu Murthy <msmurthy@msn.com>
2019-08-01 16:46:03 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php
 
+  function itms_register_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => _( 'Header Menu' ),
+        'footer-menu' => _(' Footer Menu ')
+      )
+    );
+  }
+  add_action( 'init', 'itms_register_menus' );
+
   function itms_custom_theme_assets() {
-    wp_enqueue_style('style', get_stylesheet_uri() );
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css' );
   }
   add_action( 'wp_enqueue_scripts', 'itms_custom_theme_assets' );
 
