181ded3cb26b5b89e05556c51dcc6d8a9ad34ab9
Madhu Murthy <msmurthy@msn.com>
2019-08-01 16:46:55 -0700

    Added a navigation menu and header image.

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
 
 <body <?php body_class(); ?>>
   <header class="site-header">
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) );?>
     <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
     <h4><?php bloginfo('description'); ?></h4>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/image1.jpg" alt="" />
+
   </header>
