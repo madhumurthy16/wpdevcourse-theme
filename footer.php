ac9451f88f0da358f53e9724e445007421f1946f
Madhu Murthy <msmurthy@msn.com>
2019-08-01 16:45:50 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
       <footer class="site-footer">
+        <?php wp_nav_menu( array ( 'theme_location' => 'footer-menu', 'container_class' => 'menu-footer' )); ?>
         <p><?php bloginfo('name')?> | Copyright &copy; 2019 </p>
       </footer>
     </div> <!-- closes <div class="container"> -->
-    <?php wp_footer(); ?>  
+    <?php wp_footer(); ?>
   </body>
 </html>
